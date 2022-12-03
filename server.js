a3d238a966138468e8ed5f4388ff7644f453d7a5
jose-zaragoza <zaragojf@gmatc.matc.edu>
2022-12-03 01:58:30 -0600

    Data Validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 // Initialize express application
 const app = express();
@@ -23,10 +24,30 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', (req, res) => {
+app.post(
+    '/api/users', 
+    [
+        check('name', 'Please enter your name')
+            .not()
+            .isEmpty(),
+        check('date', 'Please enter a valid date')
+            .isDate(),
+        check('mileage', 'Please enter a run mileage')
+            .isNan(),
+        check('time', 'Please enter completed run time')
+            .isNan()
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array() });
+        } else {
+          return res.send(req.body);
+        }
     console.log(req.body);
     res.send(req.body);
-    });
+    }
+);
 
 // Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
